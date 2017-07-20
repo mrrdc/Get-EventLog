1cb77bd0ee2f29fdd149cc1105960f96aba5cfdc
mrrdc <sebastian.mathes90@gmail.com>
2017-07-20 16:33:19 +0200

    Create Get-EventLog.ps1

---
+++ Get-EventLog.ps1
@@ -0,0 +1,61 @@
+#Serverliste einlesen
+$serverlist = Get-Content "\\srvfile\neuland\Abteilung\EDV\Kontrollen\Tägliche Kontrolle\Serverliste\- serverliste.txt"
+#Datumsvariablen festlegen
+$date = Get-Date
+$DatetoDelete = $date.AddDays(-30)
+$strDate = $date.ToString("yyyy-MM-dd")
+#Legt fest, welche Logs überprüft werden
+$logs = "System", "Application"
+#Anzahl der zu prüfenden Einträge
+$elements = 250
+#Log-Datei erstellen
+$file = New-Item "\\srvfile\neuland\Abteilung\EDV\Kontrollen\Tägliche Kontrolle\Eventlogs\$strDate-Events.log" -type File -force
+#SMTP Server festlegen
+$smtp = New-Object Net.Mail.SmtpClient("exchange.neuland.com")
+$message = ""
+
+
+ForEach ($servername in $serverlist) {
+    #Wenn Maschine online...
+    If (Test-Connection -Count 1 -ComputerName $servername -Quiet) {
+                    
+        Add-content $file "--------------------------"
+        Add-content $file "Ereignisauswertung: $servername"
+        Add-content $file "--------------------------"
+        # Zählen wie viele Fehler, Warnung und Informationen vorkommen
+        ForEach ($log in $logs) {            
+            $events = Get-EventLog -log $log -newest $elements -computername $servername
+            $inf_count = 0
+            $err_count = 0
+            $warn_count = 0
+            ForEach ($event in $events) {
+                If ($event.EntryType -eq "Information") {
+                    $inf_count = $inf_count + 1 
+                } ElseIf ($event.EntryType -eq "Error") {
+                    $err_count = $err_count + 1 
+                } ElseIf ($event.EntryType -eq "Warning") {
+                    $warn_count = $warn_count + 1 
+                }
+            }
+            If ($err_count -gt 42) {
+                $message = $message + "Vorsicht bei $servername, mehr als $err_count Fehler.`n"
+            }
+            #Ergebnisse in Log-Datei speichern
+            Add-content $file "Log: $log"
+            Add-content $file "Fehler:`t`t`t$err_count"
+            Add-content $file "Warnungen:`t`t$warn_count"
+            Add-content $file "Informationen:`t$inf_count`r`n"
+        } 
+    }
+    Else {
+        Add-content $file "--------------------------"
+        Add-content $file "Ereignisauswertung: $servername"
+        Add-content $file "--------------------------"
+        Add-content $file "$servername ist OFFLINE.`r`n"
+    }
+}
+If ($message -ne "") {
+    $smtp.Send("Eventlog@neuland.com","sma@neuland.com","Eventlog",$message)
+}
+#Alte Log-Dateien löschen
+Get-ChildItem "\\srvfile\neuland\Abteilung\EDV\Kontrollen\Tägliche Kontrolle\Eventlogs\" | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
